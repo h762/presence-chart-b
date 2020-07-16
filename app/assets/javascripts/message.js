4f460f63204574e3118ef4684c9fb9d332be9e33
h762 <11020i11020@gmail.com>
2020-07-16 11:16:31 +0900

    add event in A1,A2 class
    swich square and blackSquare only class:chartBtnA1 and A2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,25 @@
 $(function(){
   $('.chartBtnA1').click(function(e){
     e.preventDefault()
-    replace("lightgreen", "red");
+    var target = $(this).text();
+    if (target.match(/□/)){
+      var result = target.replace("□", "■");
+    }
+    else {
+      var result = target.replace("■", "□");
+    }
+    $(this).text(result);
+  });
+
+  $('.chartBtnA2').click(function(e){
+    e.preventDefault()
+    var target = $(this).text();
+    if (target.match(/□/)){
+      var result = target.replace("□", "■");
+    }
+    else {
+      var result = target.replace("■", "□");
+    }
+    $(this).text(result);
   });
 });
