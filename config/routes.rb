726aa6af2529f32217b6d76b91da6de1ee17cadc
h762 <11020i11020@gmail.com>
2020-07-20 19:06:55 +0900

    fix error
    before)   can't create new user
    after)   can

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'schedules#index'
-  resources :users, only: [:index, :edit, :update, :show] do
-    resource :ownpage, only: [:show]
-  end
+  resources :users, only: [:index, :edit, :update, :show]
+  # resource :ownpage, only: [:show]
   resources :schedules, only: [:index]
 end
