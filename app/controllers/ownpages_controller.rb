de4b243f161d6f07052521ec02b4ab681a3e73ca
h762 <11020i11020@gmail.com>
2020-08-18 19:59:58 +0900

    made edit page of ownpage

--- app/controllers/ownpages_controller.rb
+++ app/controllers/ownpages_controller.rb
@@ -1,23 +1,23 @@
 class OwnpagesController < ApplicationController
-  # before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :upload]
 
   def show
     # @ownpage = @user.ownpage.find(params[:id])
-    @user = User.find(params[:user_id])
-    @name = @user.name
+    # @user = User.find(params[:user_id])
+    # @name = @user.name
   end
 
   def edit
   end
 
   def upload
+    
   end
 
   private
 
     def set_user
-      @user = User.find(params[:id])
-      # @name = user.name
+      @user = User.find(params[:user_id])
+      @name = @user.name
     end
-
 end
