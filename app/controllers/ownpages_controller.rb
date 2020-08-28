396eea78a704d2e90995ec507a593b126842d9f8
h762 <11020i11020@gmail.com>
2020-08-28 19:19:08 +0900

    on the way how to divide new and edit

--- app/controllers/ownpages_controller.rb
+++ app/controllers/ownpages_controller.rb
@@ -1,9 +1,11 @@
 class OwnpagesController < ApplicationController
   before_action :set_user, only: [:new, :show, :edit, :upload]
+  # before_action :make_ownpage, only: [:show]
 
   def new
     # binding.pry
     @ownpage = Ownpage.new
+    # @ownpage.image.new
     # @ownpage = @user.ownpage.new
   end
 
@@ -12,6 +14,9 @@ class OwnpagesController < ApplicationController
   end
   
   def show
+    if Ownpage.find(current_user.id).present?
+      redirect_to new_user_ownpage_path
+    end
     @ownpage = Ownpage.find(current_user.id)
     # @ownpage = @user.ownpage.find(params[:id])
     # @user = User.find(params[:user_id])
@@ -21,6 +26,7 @@ class OwnpagesController < ApplicationController
   def edit
     # @ownpage = Ownpage.find(params[:user_id])
     @ownpage = Ownpage.find(current_user.id)
+    # @ownpage.image.new
   end
 
   def update
@@ -43,5 +49,9 @@ class OwnpagesController < ApplicationController
 
     def ownpage_params
       params.require(:ownpage).permit(:image, :content).merge(user_id: current_user.id)
+    end
+
+    def make_ownpage
+      redirect_to action: :new
     end
 end
