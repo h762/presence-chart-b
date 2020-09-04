eceb810f8f935300c133e4d5ed3c08838d79e69c
h762 <11020i11020@gmail.com>
2020-09-04 20:14:22 +0900

    on the way to use default page

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,6 +3,7 @@ class SchedulesController < ApplicationController
   def index
     @user = Schedule.new
     @user = User.all
+    # @ownpage = Ownpage.find(params[:user_id])
     @ownpage = Ownpage.find(@user.ids)
   end 
 end
