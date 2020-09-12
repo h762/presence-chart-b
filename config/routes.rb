5c41d41445ae9d20727856a1be0d849ce7b92b93
h762 <11020i11020@gmail.com>
2020-09-12 17:40:14 +0900

    clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'schedules#index'
-  # resources :users, only: [:index, :edit, :update, :show]
   resources :users, only: [:index, :new, :edit, :update, :show] do 
     resource :ownpage, only: [:new, :create, :show, :edit, :update] do
       collection do
