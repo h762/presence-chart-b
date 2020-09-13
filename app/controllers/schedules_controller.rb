81fdb86bcf1e1f167663f5ee68279d5f1f29caf0
h762 <11020i11020@gmail.com>
2020-09-13 14:31:16 +0900

    clean

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,7 +3,6 @@ class SchedulesController < ApplicationController
   def index
     @user = Schedule.new
     @user = User.all
-    # @ownpage = Ownpage.find(params[:user_id])
     # @ownpage = Ownpage.find(@user.ids)
   end 
 end
